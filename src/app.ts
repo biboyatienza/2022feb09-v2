bc083dc5ac3d719eababcdcf5d6acc41b54b0044
Biboy <biboyatienza@gmail.com>
2022-02-09 21:27:30 +0800

    Applied basic ExpressJs code

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,16 @@
-const createdBy = '-- 2022.Feb.09 v2 Ejs'
-console.log(`Game! ${createdBy}`)
+import express from 'express'
+
+const createdBy = '-- 2022.Feb.09 v2 Ejs x'
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/check', (req, res, next) => {
+    res.send(`All OK ${createdBy}`)
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Game! on port ${PORT} ${createdBy}`)
+})
